c2a2d980582782ec271d29c304351b9963db5a2a
Хохлова Наталия <nataliebasket@gmail.com>
2022-03-11 11:12:03 +0300

    small edits

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-const getInactiveStatus = () => {
+const getInactiveFormsStatus = () => {
   const adForm = document.querySelector('.ad-form');
   const mapFilter = document.querySelector('.map__filters');
   const adFormFieldsets = adForm.querySelectorAll('fieldset');
@@ -12,4 +12,20 @@ const getInactiveStatus = () => {
   mapFilterFieldsets.forEach((item) => {item.disabled = true;});
 };
 
-getInactiveStatus();
+getInactiveFormsStatus();
+
+const getActiveFormsStatus = () => {
+  const adForm = document.querySelector('.ad-form');
+  const mapFilter = document.querySelector('.map__filters');
+  const adFormFieldsets = adForm.querySelectorAll('fieldset');
+  const mapFilterFieldsets = mapFilter.querySelectorAll('fieldset');
+  const mapFilterSelects = mapFilter.querySelectorAll('select');
+
+  adForm.classList.add('ad-form--disabled');
+  mapFilter.classList.add('map__filters--disabled');
+  adFormFieldsets.forEach((item) => {item.disabled = true;});
+  mapFilterSelects.forEach((item) => {item.disabled = true;});
+  mapFilterFieldsets.forEach((item) => {item.disabled = true;});
+};
+
+getActiveFormsStatus();
