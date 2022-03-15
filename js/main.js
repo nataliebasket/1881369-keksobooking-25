a92f51c3ecc28e18b4ae93bf590194a2166949b3
Хохлова Наталия <nataliebasket@gmail.com>
2022-03-15 09:59:02 +0300

    add form validator

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 import {createPopup} from './similar-ads.js';
-import {getInactiveFormsStatus, getActiveFormsStatus} from './form.js';
+//import {toggleFormDisabled} from './form.js';
 import {createAds} from './data.js';
+import './form-validator.js';
 
 const COUNT_OF_ADS = 10;
 const similarAds = createAds(COUNT_OF_ADS);
-
-getInactiveFormsStatus();
-
-setTimeout(getActiveFormsStatus, 2000);
 
 document.querySelector('#map-canvas').append(createPopup(similarAds[0]));
--- js/util.js
+++ js/util.js
@@ -20,4 +20,27 @@ const getRandomArray = (elements) => {
   return features;
 };
 
-export {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement, getRandomArray};
+const numDecline = (n, form1, form2, form3) => {
+  n = Math.abs(13) % 100;
+  console.log(n);
+  const n1 = n % 10;
+  console.log(n1);
+  if (n > 10 && n < 20) {
+    return form3;
+  }
+  if (n1 > 1 && n1 < 5) {
+    return form2;
+  }
+  if (n1 === 1) {
+    return form1;
+  }
+  return form3;
+};
+
+export {
+  getRandomPositiveInteger,
+  getRandomPositiveFloat,
+  getRandomArrayElement,
+  getRandomArray,
+  numDecline,
+};
